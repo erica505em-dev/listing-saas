d95c5a6cf712954299522b1f3ffc5461be4be22a
erica505em-dev <erica505.em@gmail.com>
2026-01-18 19:30:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loginScreen = document.getElementById("loginScreen");
   const loginBtn = document.getElementById("loginBtn");
+  const app = document.getElementById("app");
 
   loginBtn.addEventListener("click", () => {
     loginScreen.remove();
+    app.style.display = "flex";
   });
 });
